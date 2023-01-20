e992ecbefee189e6312bafb320cee72346bf3ecb
Aakash Rao <mrao.aakash@gmail.com>
2023-01-20 15:54:38 +0530

    Added a line to print the file name

--- csv_generator.py
+++ csv_generator.py
@@ -7,7 +7,7 @@ def list_creator(path):
         for file in files:
             if file.endswith('.tif'):
                 if "HNE" in file.upper():
-                    print("HNE")
+                    print(file)
 
 if __name__ == '__main__':
     folders = ["/storage/tnbc/OPTRASCAN_IISER_Round 11"  "/storage/tnbc/OPTRASCAN_IISER_Round_10",  "/storage/tnbc/OPTRASCAN_IISER_Round_2",  "/storage/tnbc/OPTRASCAN_IISER_Round_6",  "/storage/tnbc/OPTRASCAN_IISER_Round_8",  "/storage/tnbc/OPTRASCAN_IISER_Round_1",   "/storage/tnbc/OPTRASCAN_IISER_Round_12",  "/storage/tnbc/OPTRASCAN_IISER_Round_3",  "/storage/tnbc/OPTRASCAN_IISER_Round_7",  "/storage/tnbc/OPTRASCAN_IISER_Round_9" ]
