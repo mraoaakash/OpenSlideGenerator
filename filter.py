6efbf1ab7b34e3e52f9691fd99f3a436f6d13bac
Aakash Rao <mrao.aakash@gmail.com>
2023-01-23 11:08:13 +0530

    removed double loop, which was causing /images/images error

--- filter.py
+++ filter.py
@@ -5,33 +5,33 @@ import shutil
 import numpy as np
 import joblib
 from Differentiator.hardcode_tester import predict
+import warnings
+warnings.filterwarnings("ignore")
 
 def filter(path):
-    for i in os.listdir(path):
-        print(f'Filtering {i}')
-        new_path = os.path.join(path, i)
-        if not os.path.isdir(os.path.join(new_path, 'rejected')):
-            os.makedirs(os.path.join(new_path, 'rejected'))
-        rejected = os.path.join(new_path, 'rejected')
-        # print(rejected)
-        if not os.path.isdir(os.path.join(new_path, 'accepted')):
-            os.makedirs(os.path.join(new_path, 'accepted'))
-        accepted = os.path.join(new_path, 'accepted')
-        # print(accepted)
-        model = joblib.load('/storage/bic/data/breastCancer/OpenSlideGenerator/Differentiator/finalized_model.sav')
-        # print('Model loaded')
-        for image in os.listdir(os.path.join(new_path, 'images')):
-            image_path = os.path.join(new_path, 'images', image)
-            # print(image_path)
-            data = np.array(predict(image_path)).reshape(1, -1)
-            prediction = model.predict(data)
-            # print(prediction[0])
-            if prediction == 'cellSpace':
-                # print(f'Accepted {image_path}')
-                shutil.copy(image_path, accepted)
-            else:
-                print (f'Rejected {image_path}')
-                shutil.copy(image_path, rejected)
+    new_path = path
+    if not os.path.isdir(os.path.join(new_path, 'rejected')):
+        os.makedirs(os.path.join(new_path, 'rejected'))
+    rejected = os.path.join(new_path, 'rejected')
+    # print(rejected)
+    if not os.path.isdir(os.path.join(new_path, 'accepted')):
+        os.makedirs(os.path.join(new_path, 'accepted'))
+    accepted = os.path.join(new_path, 'accepted')
+    # print(accepted)
+    model = joblib.load('/storage/bic/data/breastCancer/OpenSlideGenerator/Differentiator/finalized_model.sav')
+    # print('Model loaded')
+    for image in os.listdir(os.path.join(new_path, 'images')):
+        image_path = os.path.join(new_path, 'images', image)
+        # print(image_path)
+        data = np.array(predict(image_path)).reshape(1, -1)
+        prediction = model.predict(data)
+        # print(prediction[0])
+        if prediction == 'cellSpace':
+            # print(f'Accepted {image_path}')
+            shutil.copy(image_path, accepted)
+        else:
+            print (f'Rejected {image_path}')
+            shutil.copy(image_path, rejected)
     return
 
 def list_creator(path, path_new_level):
